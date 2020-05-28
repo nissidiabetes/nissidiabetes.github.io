c089cffa561f082bb571861877db18b48b4a59e7
oa <info@en-gnr.com>
2020-05-28 15:17:30 -0400

    full site fix

--- testsite/js/scripts.js
+++ testsite/js/scripts.js
@@ -1,39 +1,72 @@
+// Hamburger
 $(document).ready(function() {
-  // Look for .hamburger
-  var hamburger = document.querySelector(".hamburger");
-  // On click
-  hamburger.addEventListener("click", function() {
-    // Toggle class "is-active"
-    hamburger.classList.toggle("is-active");
-    // Do something else, like open/close menu
-    $('body').toggleClass("overflow-hidden");
-    //$('.logo-link-light').toggle();
-    //$('.logo-link-dark').toggle();
-    $('.mobile-nav').toggleClass("open");
 
+  var hamburger = document.querySelector(".hamburger");
+  hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle("is-active");
+    $('body').toggleClass("overflow-hidden");
+    $('.mobile-nav').toggleClass("open");
   });
+
+
+  $('.mobile-link').click(function(){
+    $('.mobile-nav').removeClass("open");
+    $('.hamburger').removeClass("is-active");
+  });
+
 });
 
 
 
 
-
-
-let servicesHeight = $('.nissi-services').height();
-let servicesHeightAdj = servicesHeight - 120;
-
+// Home Page Height
 $(document).ready(function(){
     $('.nissi-home').css('height', $(window).height());
-    //$('.services-outer').css('height', servicesHeightAdj);
-
-    // Comma, not colon ----^
 });
 $(window).resize(function(){
     $('.nissi-home').css('height', $(window).height());
-    //$('.services-outer').css('height', servicesHeightAdj);
-
-    // Comma, not colon ----^
 });
 
 
+// Copyright Year
 $(".year").text( (new Date).getFullYear() );
+
+
+
+// Smooth Scroll
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
